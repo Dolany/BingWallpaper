8e9a83cfeda852b47a66ad25ce0a8f3847660508
Dolany <dolany@sina.cn>
2020-01-28 21:02:26 +0800

    update

--- BgImg/Program.cs
+++ BgImg/Program.cs
@@ -8,15 +8,29 @@ namespace BgImg
 {
     class Program
     {
+        private const string PicCachePath = "./BGs/";
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Program is starting...");
+            Console.WriteLine("Parsing Url...");
             var fullPath = BingBgImgSvc.GetUrl();
-            var savePath = "./" + DateTime.Now.ToString("yyyyMMdd") + ".jpg";
+            var dir = new DirectoryInfo(PicCachePath);
+            if (!dir.Exists)
+            {
+                Console.WriteLine("Creating PicCache Folder...");
+                dir.Create();
+            }
+
+            var savePath = $"{PicCachePath}{DateTime.Now:yyyyMMdd}.jpg";
+            Console.WriteLine("Downloading Image...");
             Utility.Common.DownloadImage(fullPath, savePath);
             var image = Image.FromFile(savePath);
+            Console.WriteLine("Copying Image to Workplace...");
             image.Save("./desktopBgImg.bmp", ImageFormat.Bmp);
 
             var file = new FileInfo("./desktopBgImg.bmp");
+            Console.WriteLine("Setting Bg...");
             Utility.Common.SystemParametersInfo(20, 0, file.FullName, 0x2);
 
             Console.WriteLine("Completed");
