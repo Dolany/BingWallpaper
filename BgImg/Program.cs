068a2686e41f82f76047862c68bc2afb97f42b39
Dolany <dolany@sina.cn>
2020-02-08 19:56:33 +0800

    try catch

--- BgImg/Program.cs
+++ BgImg/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using BingService;
+using Utility;
 
 namespace BgImg
 {
@@ -14,26 +15,36 @@ namespace BgImg
         {
             Console.WriteLine("Program is starting...");
             Console.WriteLine("Parsing Url...");
-            var fullPath = BingBgImgSvc.GetUrl();
-            var dir = new DirectoryInfo(PicCachePath);
-            if (!dir.Exists)
+            try
             {
-                Console.WriteLine("Creating PicCache Folder...");
-                dir.Create();
+                var fullPath = BingBgImgSvc.GetUrl();
+                var dir = new DirectoryInfo(PicCachePath);
+                if (!dir.Exists)
+                {
+                    Console.WriteLine("Creating PicCache Folder...");
+                    dir.Create();
+                }
+
+                var savePath = $"{PicCachePath}{DateTime.Now:yyyyMMdd}.jpg";
+                Console.WriteLine("Downloading Image...");
+                Common.DownloadImage(fullPath, savePath);
+                var image = Image.FromFile(savePath);
+                Console.WriteLine("Copying Image to Workplace...");
+                image.Save("./desktopBgImg.bmp", ImageFormat.Bmp);
+
+                var file = new FileInfo("./desktopBgImg.bmp");
+                Console.WriteLine("Setting Bg...");
+                Common.SystemParametersInfo(20, 0, file.FullName, 0x2);
+
+                Console.WriteLine("Completed");
             }
-
-            var savePath = $"{PicCachePath}{DateTime.Now:yyyyMMdd}.jpg";
-            Console.WriteLine("Downloading Image...");
-            Utility.Common.DownloadImage(fullPath, savePath);
-            var image = Image.FromFile(savePath);
-            Console.WriteLine("Copying Image to Workplace...");
-            image.Save("./desktopBgImg.bmp", ImageFormat.Bmp);
-
-            var file = new FileInfo("./desktopBgImg.bmp");
-            Console.WriteLine("Setting Bg...");
-            Utility.Common.SystemParametersInfo(20, 0, file.FullName, 0x2);
-
-            Console.WriteLine("Completed");
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong...");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                throw;
+            }
             Console.ReadKey();
         }
     }
